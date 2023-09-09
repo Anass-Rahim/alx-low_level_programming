2d47b0a9c2dde2def323398fc22af3d910db84c1
Anass-Rahim <anassibrahimma@gmail.com>
2023-09-09 14:38:30 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-    // Print the message using the puts function
+int main(void)
+{
+    
     puts("Programming is like building a multilingual puzzle");
 
-    return 0; // Return 0 to indicate successful execution
+    return (0);
 }
