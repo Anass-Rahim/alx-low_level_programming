77d28631dfac9ce4491f55ecd7003277b77511d9
Anass-Rahim <anassibrahimma@gmail.com>
2023-09-09 13:46:10 -0700

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("size of the char: %d byte(s)\n", sizeo(char));
+	printf("size of an int: %d byte(s)\n", sizeo(int));
+	printf("size of a long int: %d byte(s)\n", sizeo(long int));
+	printf("size of a long int: %d byte(s)\n", sizeo(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeo(long long int));
+    	return (0);
+}
