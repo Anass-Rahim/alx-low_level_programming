ddd8d0cbfbfe3ccd7dee842621198fa2d0d4cb6c
Anass-Rahim <anassibrahimma@gmail.com>
2023-09-12 10:09:21 -0700

    c programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and 
+ * Returns: the result.
+ */
+int add(int, int);
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * Return: the absolute value of an integer.
+ */
+int _abs(int);
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
