f5ef42c1e132bd539253d09c1fc5b3d08b6f7d4c
Anass-Rahim <anassibrahimma@gmail.com>
2023-09-14 12:14:43 -0700

    more c programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 
 void more_numbers(void)
-{       
-        char i, j;
-        
-        for (i = 1; i <= 10; i++)
-        {       
-                for (j = 0; j <= 14; j++)
-                {       
-                        if (j >= 10)
-                        {
-                                _putchar('1');
-                        }       
-                        _putchar((j % 10) + '0');
+{
+	char i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
